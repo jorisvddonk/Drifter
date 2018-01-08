f5adf4c31ccecd4d32e3682b840beab9bfbebd49
Joris van de Donk <jorisvddonk@gmail.com>
2018-01-08 21:15:43 +0100

    Uglify production builds

--- webpack.conf.js
+++ webpack.conf.js
@@ -1,7 +1,14 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+var plugins = [];
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(new UglifyJsPlugin({}));
+}
+
 module.exports = {
   entry: './index.js',
   output: {
     filename: 'dist/bundle.js'
   },
-  plugins: []
+  plugins: plugins
 };
