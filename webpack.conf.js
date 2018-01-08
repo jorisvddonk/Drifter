1ac0a6aaadd07772de697724d66d4e512c801a16
Joris van de Donk <jorisvddonk@gmail.com>
2018-01-08 21:09:53 +0100

    Removed livereload plugin

--- webpack.conf.js
+++ webpack.conf.js
@@ -1,11 +1,7 @@
-var LiveReloadPlugin = require('webpack-livereload-plugin');
-
 module.exports = {
   entry: './index.js',
   output: {
     filename: 'dist/bundle.js'
   },
-  plugins: [
-    new LiveReloadPlugin({})
-  ]
+  plugins: []
 };
