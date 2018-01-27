dd6fe74e15e275ab18075350c24114a712038f36
joris <jorisvddonk@gmail.com>
2018-01-27 18:06:27 +0100

    Show the sun in the sky

--- shaders/skyGradient.js
+++ shaders/skyGradient.js
@@ -2,7 +2,7 @@
 /*
 The MIT License
 
-Copyright © 2015-2017 A-Frame authors.
+Copyright © 2015-2017 A-Frame authors & Joris van de Donk.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -26,7 +26,8 @@ THE SOFTWARE.
 AFRAME.registerShader('skyGradient', {
   schema: {
     colorTop: { type: 'color', default: 'black', is: 'uniform' },
-    colorBottom: { type: 'color', default: 'red', is: 'uniform' }
+    colorBottom: { type: 'color', default: 'red', is: 'uniform' },
+    sunPosition: { type: 'vec3', default: '1 1 0', is: 'uniform' }
   },
 
   vertexShader: `
@@ -49,6 +50,15 @@ AFRAME.registerShader('skyGradient', {
    uniform vec3 sunPosition;
 
    varying vec3 vWorldPosition;
+   const vec3 sun = vec3(1.0, 1.0, 0.0);
+
+   float sunEffect(vec3 pointOnSphere) {
+     float angle = acos(dot(pointOnSphere, normalize(sunPosition)));
+     if (angle <= 0.03) {
+       return 1.0;
+     }
+     return 0.0;
+   }
 
    void main()
 
@@ -57,5 +67,7 @@ AFRAME.registerShader('skyGradient', {
      float f = 1.0;
      f = sin((pointOnSphere.y+0.0) * 1.5);
      gl_FragColor = vec4(mix(colorBottom,colorTop, f ), 1.0);
+     float sunnyness = sunEffect(pointOnSphere);
+     gl_FragColor = vec4(gl_FragColor.r + sunnyness, gl_FragColor.g + sunnyness, gl_FragColor.b + sunnyness, gl_FragColor.a);
    }`
 });
