7b5e4bed0746d770273550d720c76260464cb7b0
joris <jorisvddonk@gmail.com>
2018-01-28 16:37:27 +0100

    Remove 'remove-hand-controls' component when it's no longer needed

--- src/drifter-components.js
+++ src/drifter-components.js
@@ -236,10 +236,10 @@ AFRAME.registerComponent('remove-hand-controls', {
   init: function() {},
   tick: function(time) {
     // remove default hand model
-    // TODO: improve performance.
     var obj3d = this.el.getObject3D('mesh');
     if (obj3d) {
       this.el.removeObject3D('mesh');
+      this.el.removeAttribute('remove-hand-controls'); // once default hand model is removed; remove this component
     }
   }
 });
