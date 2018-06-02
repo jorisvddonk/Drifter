a41bb52b8eaabae1f1e7b5baec908362d199162f
joris <jorisvddonk@gmail.com>
2018-06-02 21:30:48 +0200

    Added ncc-model component, which can load a Noctis .ncc model

--- src/drifter-components.js
+++ src/drifter-components.js
@@ -15,7 +15,6 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 // renderPlanetTexture(ruinschart, 200, 200, 2, false); // render planet ruins chart (ruins on Felysian worlds, lava)
 // renderPlanetTexture(txtr,256,256,2); // render surface texture
 // renderPalette(); // render palette
-
 var url = require('url');
 var xinited = false;
 var RIGHT_HAND_TOOLS = ['none', 'map', 'texture_surface', 'texture_planet'];
@@ -157,6 +156,88 @@ var xinit = function() {
   }
 };
 
+AFRAME.registerComponent('ncc-model', {
+  schema: {
+    filename: { default: '', type: 'string' }
+  },
+  init: function() {
+    fetch(this.data.filename)
+      .then(function(res) {
+        return res.arrayBuffer();
+      })
+      .then(buffer => {
+        this.geometry = new THREE.Geometry();
+        var dataView = new DataView(buffer);
+        var offset = 0;
+        var readUInt8 = function() {
+          var retval = dataView.getUint8(offset, true);
+          offset += 1;
+          return retval;
+        };
+        var readFloat32 = function() {
+          var retval = dataView.getFloat32(offset, true);
+          offset += 4;
+          return retval;
+        };
+        var num_polys = readUInt8();
+        offset += 1; // huh?
+        var num_vertices_per_poly = [];
+        var xs = [];
+        var ys = [];
+        var zs = [];
+        var colors = [];
+        for (var i = 0; i < num_polys; i++) {
+          num_vertices_per_poly.push(readUInt8());
+        }
+        for (var i = 0; i < num_polys * 4; i++) {
+          xs.push(readFloat32());
+        }
+        for (var i = 0; i < num_polys * 4; i++) {
+          ys.push(readFloat32());
+        }
+        for (var i = 0; i < num_polys * 4; i++) {
+          zs.push(readFloat32());
+        }
+        for (var i = 0; i < num_polys; i++) {
+          colors.push(readUInt8());
+        }
+        var pindex = 0;
+        for (var i = 0; i < num_polys; i++) {
+          for (var j = 0; j < num_vertices_per_poly[i]; j++) {
+            this.geometry.vertices.push(
+              new THREE.Vector3(xs[i * 4 + j], ys[i * 4 + j], zs[i * 4 + j])
+            );
+          }
+          if (num_vertices_per_poly[i] == 3) {
+            this.geometry.faces.push(
+              new THREE.Face3(pindex + 0, pindex + 1, pindex + 2)
+            );
+          } else if (num_vertices_per_poly[i] == 4) {
+            this.geometry.faces.push(
+              new THREE.Face3(pindex + 0, pindex + 1, pindex + 2)
+            );
+            this.geometry.faces.push(
+              new THREE.Face3(pindex + 0, pindex + 2, pindex + 3)
+            );
+          }
+          pindex = pindex + num_vertices_per_poly[i];
+        }
+        this.geometry.scale(0.01, 0.01, 0.01);
+        this.geometry.rotateY(Math.PI * 0.5);
+        this.geometry.rotateZ(Math.PI * 1);
+        this.geometry.computeVertexNormals();
+        this.geometry.computeFaceNormals();
+        this.geometry.computeBoundingSphere();
+        this.material = new THREE.MeshStandardMaterial({
+          color: '#fff',
+          side: THREE.DoubleSide
+        });
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
+        this.el.setObject3D('mesh', this.mesh);
+      });
+  }
+});
+
 AFRAME.registerGeometry('planetsurface', {
   schema: {
     xmin: { default: 0, min: 0, max: 200, type: 'int' },
@@ -209,6 +290,19 @@ var toHex = function(i) {
   return v;
 };
 
+var getSunColor = function() {
+  var r = Math.min(255, nearstar_r * 4);
+  var g = Math.min(255, nearstar_g * 4);
+  var b = Math.min(255, nearstar_b * 4);
+  r = parseInt(r);
+  g = parseInt(g);
+  b = parseInt(b);
+  r = toHex(r);
+  g = toHex(g);
+  b = toHex(b);
+  return '#' + r + g + b;
+};
+
 var getSkyHexColor = function() {
   var atmosphericDensity = planet_typesAtmosphericDensity[PLANET_TYPE];
   var r = Math.min(255, nearstar_r * 4);
@@ -240,8 +334,9 @@ AFRAME.registerComponent('planet-sky', {
     var r = toHex(Math.min(255, nearstar_r * 4));
     var g = toHex(Math.min(255, nearstar_g * 4));
     var b = toHex(Math.min(255, nearstar_b * 4));
+    /*var sunColor = getSunColor();
+    this.el.setAttribute('material', 'sunColor', sunColor);*/
     var skyColor = getSkyHexColor();
-
     this.el.setAttribute('material', 'colorTop', skyColor);
     this.el.setAttribute('material', 'colorBottom', skyColor);
     this.el.setAttribute('material', 'sunscattering', sunscattering);
